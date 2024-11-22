2ce56e6230d27045baf6dc5a0b1d389235545999
Tom <tom@matterfi.com>
2024-11-22 10:13:52 -0600

    add ios x64 simulator triplet

---
+++ triplets/x64-ios-simulator.cmake
@@ -0,0 +1,8 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME iOS)
+set(VCPKG_OSX_SYSROOT iphonesimulator)
+
+set(SDKROOT "$ENV{XCODE_ROOT}/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk")
+set(ENV{SDKROOT} "${SDKROOT}")
